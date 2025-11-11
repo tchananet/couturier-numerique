a976e7a23b12874eae5f8d680ab03bb6af87a1be
ALPHA MOTORS <contact@alphamotors-cameroun.com>
2025-11-11 03:30:11 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/order-details.tsx
+++ src/components/order-details.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import * as React from "react";
 import { OrderWithClient, formatCurrency } from "@/lib/data";
 import { Badge } from "@/components/ui/badge";
 import { Separator } from "@/components/ui/separator";
